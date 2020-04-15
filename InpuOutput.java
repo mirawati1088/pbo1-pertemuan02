653c37894ca78922f3f33e9e4ac41bf1c627e70e
mirawati1088 <mira.dhibah@gmail.com>
2020-04-14 21:44:52 -0700

    Buat Output

--- InpuOutput.java
+++ InpuOutput.java
@@ -1,8 +1,11 @@
 import java.until.Scanner;
 
 public class InputOutput{
-    public static void main (String [] args){
-         Scanner scaner = new Scanner(System.in);
-    }
-   
+ public static void main (String [] args){
+  Scanner scaner = new Scanner(System.in);
+  String nama;
+  nama = scaner.nextLine();
+         
+
+    }   
 }
